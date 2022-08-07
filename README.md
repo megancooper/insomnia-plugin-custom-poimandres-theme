081577c91c8a185de90d9652930e2625911afc4e
Megan Cooper <megancooper1000@gmail.com>
2022-08-06 18:34:04 -0700

    feat: create poimandres-ish theme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # insomnia-plugin-custom-poimandres-theme
 A custom poimandres theme for insomnia
+
+## About
+
+This is a custom poimandres-ish theme for insomnia. I really like the vscode [poimandres](https://github.com/drcmda/poimandres-theme) and wanted a similar color scheme inside insomnia.
+
+## Contributing
+
+Contributions are welcome! This theme is rough, and I chose a stopping point that was sufficient for the areas of insomnia I use daily. Feel free to fork and improve!
+
+## Installation
+
---
+++ index.js
@@ -0,0 +1,66 @@
+module.exports.themes = [
+  {
+    name: "custom-poimandres",
+    displayName: "Custom Poimandres",
+    theme: {
+      background: {
+        default: "#1b1e28",
+        success: "#add7FF",
+        notice: "#5de4c7",
+        warning: "#fffac2",
+        danger: "#fffac2",
+        surprise: "#f087bd",
+        info: "#add7ff",
+      },
+      foreground: {
+        default: "#7390aa",
+        success: "#333",
+        notice: "#000",
+        warning: "#333",
+        danger: "#333",
+        surprise: "#000",
+        info: "#000",
+      },
+      highlight: {
+        default: "#506477",
+      },
+
+      styles: {
+        appHeader: {
+          foreground: {
+            surprise: "#fff",
+          },
+          background: {
+            surprise: "#303340",
+          },
+        },
+        sidebarHeader: {
+          foreground: {
+            surprise: "#fff",
+          },
+          background: {
+            surprise: "#303340",
+          },
+        },
+        paneHeader: {
+          foreground: {
+            surprise: "#fff",
+          },
+          background: {
+            surprise: "#303340",
+          },
+        },
+        dialog: {
+          background: {
+            default: "#1b1e28",
+            surprise: "#303340",
+          },
+          foreground: {
+            default: "#91b4d5",
+            surprise: "#fff",
+          },
+        },
+      },
+    },
+  },
+];
